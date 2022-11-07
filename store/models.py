a6a97a95661509f76dafe99a2f313be334a5383d
Mohammed Hakmi <mohammed.hakmiii555@gmail.com>
2022-11-07 19:19:56 +0100

    creating store orm modules and defining relationships

--- store/models.py
+++ store/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  price = models.DecimalField(max_digits= 8, decimal_places=2)
+  invetory = models.IntegerField()
+  last_update = models.DateTimeField(auto_now= True)
+
+class Customer(models.Model):
+  MEMBERSHIP_BRONZE = 'B'
+  MEMBERSHIP_SLIVER = 'S'
+  MEMBERSHIP_GOLD = 'G'
+
+  # Membership types 
+  MEMBERSHIP_CHOICES= [
+    (MEMBERSHIP_BRONZE, 'Bronze'),
+    (MEMBERSHIP_SLIVER, 'Bronze'),
+    (MEMBERSHIP_GOLD, 'Bronze'),
+  ]
+  first_name = models.CharField(max_length = 255)
+  last_name = models.CharField(max_length = 255)
+  email = models.EmailField(unique= True)
+  phone = models.CharField(max_length= 255)
+  birth_date = models.DateTimeField(null= True)
+  membership = models.CharField(max_length= 1, choices= MEMBERSHIP_CHOICES, default= MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+  PAYMENT_STATUS_PENDING = 'P'
+  PAYMENT_STATUS_COMPLETED = 'C'
+  PAYMENT_STATUS_FAILED = 'F'
+
+  PAYMENT_STATUS_CHOICES= [
+    (PAYMENT_STATUS_PENDING, 'Pending'),
+    (PAYMENT_STATUS_COMPLETED, 'Complete'),
+    (PAYMENT_STATUS_FAILED, 'Failed'),
+  ]
+  placed_at = models.DateTimeField(auto_now_add= True)
+  payment_status = models.CharField(max_length= 1, choices= PAYMENT_STATUS_CHOICES, default= PAYMENT_STATUS_PENDING)
+
+class Address(models.Model):
+  street = models.CharField(max_length= 255)
+  city= models.CharField(max_length= 255)
+  customer= models.OneToOneField(Customer, on_delete= models.CASCADE, primary_key= True) 
